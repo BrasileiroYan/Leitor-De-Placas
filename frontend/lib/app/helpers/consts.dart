fb5c7e92156af205077d053efa3699a9008131ab
Oto Serafim <186635411+otors@users.noreply.github.com>
2025-06-25 01:13:59 -0300

    refactor

--- frontend/lib/app/helpers/consts.dart
+++
@@ -1,8 +0,0 @@
-import 'package:frontend/app/api_key.dart';
-
-const String baseApiUrl = 'http://localhost:8080';
-
-const String vehicleTestUrl =
-    "https://api.github.com/gists/02a542af28f8ab8c06ef41203016af53";
-
-const requestHeader = {'Authorization': 'Bearer $apiKey'};
